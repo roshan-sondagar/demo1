48191341dd7424d0c4e12c2451c3f1af7f61c507
roshan-sondagar <roshanbsondager@gmail.com>
2026-05-22 15:45:33 +0530

    practice questions

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-let marks = [36 , 55, 44, 49 , 78, 93.14];
+let n = prompt("enter a number : ");
 
-let topper = marks.filter((marks)=> {
-    return marks >= 90;
+let arr = [];
+
+for (i = 1; i <= n; i++) {
+    arr[i - 1] = i;
+}
+
+console.log(arr);
+
+let sum = arr.reduce ((prev , curr) => {
+    return prev + curr;
 });
 
-console.log(topper);
+console.log(sum);
+
+let factorial = arr.reduce ((prev , curr)=>{
+    return prev * curr;
+});
+
+console.log(factorial);
